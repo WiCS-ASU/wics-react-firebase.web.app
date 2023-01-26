d3f8af7c35548399d42c057ef409eb27b59f53b9
Leyli Ghavami <leylighavami@gmail.com>
2023-01-26 00:00:39 -0500

    updated sponsors info

--- src/Sponsor Tiers/BronzeTier.js
+++ src/Sponsor Tiers/BronzeTier.js
@@ -2,40 +2,31 @@ import { Container, Row, Col } from "react-bootstrap";
 import Sponsorships from "../UI/Cards/Sponsorships";
 
 function BronzeTier() {
-    return (
-<div className="bg-light"> 
-            <Container>
-                
-                <h1 className="bronzeTier text-center p-4">⎯⎯⎯⎯ Bronze Tier ⎯⎯⎯⎯</h1>
-                
-                <Row>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "GoDaddy"
-                            color = "bg-danger"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Carvana"
-                            color = "bg-danger"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Edward Jones"
-                            color = "bg-danger"
-                        />
-                    </Col>
-                 
-                </Row>
-                
-            </Container>
-        </div>
-    );
+  return (
+    <div className="bg-light">
+      <Container>
+        <h1 className="bronzeTier text-center p-4">⎯⎯⎯⎯ Bronze Tier ⎯⎯⎯⎯</h1>
+
+        <Row>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Viasat" color="bg-danger" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="L3 Harris Technology" color="bg-danger" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Bloomberg" color="bg-danger" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Aveva" color="bg-danger" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="GoDaddy" color="bg-danger" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default BronzeTier;
--- src/Sponsor Tiers/SilverTier.js
+++ src/Sponsor Tiers/SilverTier.js
@@ -2,82 +2,43 @@ import { Container, Row, Col } from "react-bootstrap";
 import Sponsorships from "../UI/Cards/Sponsorships";
 
 function SilverTier() {
-    return (
-<div className="bg-light"> 
-            <Container>
-                
-                <h1 className="silverTier text-center p-4">⎯⎯⎯⎯ Silver Tier ⎯⎯⎯⎯</h1>
-                
-                <Row>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Octo"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Amazon"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Paradox"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "USAA"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Workiva"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Goldman Sachs"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "Fox"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "State Farm"
-                            color= "bg-success"
-                        />
-                    </Col>
-                    <Col md={6} className="my-3">
-                        
-                        <Sponsorships 
-                            title = "General Dynamics Mission Systems"
-                            color= "bg-success"
-                        />
-                    </Col>
-                   
-                </Row>
-                
-            </Container>
-        </div>
-    );
+  return (
+    <div className="bg-light">
+      <Container>
+        <h1 className="silverTier text-center p-4">⎯⎯⎯⎯ Silver Tier ⎯⎯⎯⎯</h1>
+
+        <Row>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Amazon" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Chime" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="USAA" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Workiva" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Goldman Sachs" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="Fox" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships title="State Farm" color="bg-success" />
+          </Col>
+          <Col md={6} className="my-3">
+            <Sponsorships
+              title="General Dynamics Mission Systems"
+              color="bg-success"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default SilverTier;
