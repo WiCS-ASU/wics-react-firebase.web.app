d5182ab4d0f9d30eda418e59a0381943fb1e5ae0
Michelle Tam <tammichelle830@gmail.com>
2025-04-17 02:10:34 -0700

    Temp Update for 2025

---
+++ src/Pages/PastCompetitions.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Table } from "react-bootstrap";
+
+function PastCompetitions() {
+    return (
+        <div>
+            <h1>Past Competitions</h1>
+            <p>Details about past competitions will be displayed here.</p>
+        </div>
+    );
+} 
+
+export default PastCompetitions;
--- src/UI/Cards/WWD.js
+++ src/UI/Cards/WWD.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Card, Carousel } from 'react-bootstrap';
 
-// height 415px
+
 function WWD({ title, description, images }) {
   return (
     <Card className="mx-auto">
       <Card.Body className="d-flex flex-column align-items-center">
-        <Card.Title className="text-center">{title}</Card.Title>
-        <Carousel className="text-center" interval={null} style={{ height: "350px", padding: "50px" }}>
+        <Card.Title className="text-center" 
+        style={{ marginBottom: "-10px" }}>{title}</Card.Title>
+        <Carousel className="text-center WhatWeDo-title" interval={null} style={{ height: "350px", width: "100%", padding: "10px"}}>
           {images.map((image, index) => (
             <Carousel.Item key={index}>
               <img
-                /* className="w-100" */
                 src={image.src}
                 alt={image.alt}
-                style={{ width: "100%", height: "600px", objectFit: "cover", padding: "0px" }}
+                style={{
+                  width: "auto", // Maintain aspect ratio
+                  height: "100%", // Fill height of the container
+                  objectFit: "cover", // Ensure proper cropping
+                  display: "block", // Ensure the image behaves like a block element
+                  margin: "22px auto", // Center horizontally
+                }}
               />
             </Carousel.Item>
           ))}
-        </Carousel >
-        <Card.Text className="text-center mt-3">
+        </Carousel>
+        <Card.Text className="text-center mt-3 WhatWeDo-description">
           {description}
         </Card.Text>
       </Card.Body>
