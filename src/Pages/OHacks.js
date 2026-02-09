0c363fd04246124cd361e20655b431b00b272bd4
poojithanuthalapati <poojitha060905@gmail.com>
2026-02-08 17:12:46 -0700

    Create OHacks.js

---
+++ src/Pages/OHacks.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Table } from "react-bootstrap";
+
+
+function OHacks() {
+  return (
+    <p>
+                  Apply  <a href="https://docs.google.com/forms/d/e/1FAIpQLSerF9LnsUV5Q2dcWopuCyEQ5MfXCuWhupQC8-d27SE7QVIbrw/viewform">here </a>
+                  for the hackathon! Please note that the $15
+                  deposit to our PayPal must be made before you are officially
+                  registered. You will get the deposit returned to you after the
+                  competition. If you are a no-show to the competition, you will
+                  NOT receive the deposit back.
+                </p>
+  );
+}
